8c2b840963027b609066ba8cdcb53614e0912afe
sehomi <sehomi755@gmail.com>
2023-12-11 21:28:13 +0000

    training for licience plate done + models added

--- train.py
+++
@@ -1,8 +0,0 @@
- 
-import utils
-from ultralytics import YOLO
-
-utils.convert_dataset("/content/ANPR/dataset/car-plate-detection/")
-
-model = YOLO('yolov8n.yaml')
-result = model.train(data="/content/ANPR/dataset/car-plate-detection/config.yaml",device="0",epochs=100,verbose=True,plots=True,save=True)
